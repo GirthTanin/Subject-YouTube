633046e17396ddaf0cc30b2aed2e34b870e431eb
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-10-09 15:22:06 -0400

    here is the server.js

--- subject-youtube/server.js
+++ subject-youtube/server.js
@@ -1,5 +1,8 @@
-const express = require ('express');
+// Not entirely sure how this works, but I'm terrible at keeping keys etc safe.
+require("dotenv").config();
 
+const express = require ('express');
+// do we want "path" as a const here also?  What is it's role?
 const mongoose = require ("mongoose");
 const routes = require ("./routes");
 const app = express();
@@ -15,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
+    process.env.MONGODB_URI || "mongodb://localhost/subject-youtube"
 );
 
 app.listen(PORT, function() {
