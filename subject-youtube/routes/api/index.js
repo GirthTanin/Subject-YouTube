a0d0bb60ea767301b1dca9d9e2760ae4dc5370ff
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-10-13 22:52:58 -0400

    these routes are not as clear as last time

---
+++ subject-youtube/routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+// I am not sure of the layout of all the routes...
+const subjectRoutes = require("./subject");
+
+// subject routes
+router.use("/subject", subjectRoutes);
+
+module.exports = router;
