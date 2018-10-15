f62e3f27b4421c455d91afffd23afedabe182255
Kyle Carney <37038001+kylecarney@users.noreply.github.com>
2018-10-15 18:55:53 -0400

    Revert "work flow cheat sheet"

--- subject-youtube/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-// I am not sure of the layout of all the routes...
-const subjectRoutes = require("./subject");
-
-// subject routes
-router.use("/subject", subjectRoutes);
-
-module.exports = router;
