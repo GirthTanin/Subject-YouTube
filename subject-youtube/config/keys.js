277629b63f35fc958044e4dddbbe2c8b6ef78149
Kyle Carney <kyle.e.carney@gmail.com>
2018-10-14 21:35:14 -0400

    update keys.js

---
+++ subject-youtube/config/keys.js
@@ -0,0 +1,25 @@
+//add this file to .gitignore
+
+module.exports = {
+   google: {
+       clientID:'11209142647-aq2t28242p6r0uq6pd9lgou6nir2h6qt.apps.googleusercontent.com',
+       clientSecret: 'P9RfTX-r9NQFWGEeYPGEk42g'
+        }
+   };
+
+   // add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '11209142647-aq2t28242p6r0uq6pd9lgou6nir2h6qt.apps.googleusercontent.com',
+        clientSecret: 'P9RfTX-r9NQFWGEeYPGEk42g'
+    },
+    mongodb: {
+        //need mongodb info example: dbURI: 'mongodb://iamshaunjp:test6@ds151024.mlab.com:51024/oauth-test'
+        dbURI: 
+        'mongodb://iamshaunjp:test6@ds151024.mlab.com:51024/oauth-test'
+    },
+    session: {
+        cookieKey: 'thenetninjaisawesomeiguess'
+    }
+};
