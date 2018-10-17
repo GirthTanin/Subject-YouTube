dffd3ded3a51cf66b0669fbe2447a8ab3eced006
Kyle Carney <kyle.e.carney@gmail.com>
2018-10-16 21:41:58 -0400

    update navjs with navbar

--- subject-youtube/client/src/App.js
+++ subject-youtube/client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Nav from './components/NavBar/Nav.js';
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
--- subject-youtube/config/keys.js
+++ subject-youtube/config/keys.js
@@ -2,7 +2,11 @@
 
 module.exports = {
    google: {
+<<<<<<< HEAD
        clientID:'11209142647-aq2t28242p6r0uq6pd9lgou6nir2h6qt.apps.googleusercontent.com',
+=======
+       clientID:'11209142647-aq2t28242p6r0uq6pd9lgou6nir2h6qt.apps.googleusercontent.com'
+>>>>>>> 5a6025bb5736900c9ade03b0fabc67f4fd2b81bd
        clientSecret: 'P9RfTX-r9NQFWGEeYPGEk42g'
         }
    };
@@ -22,4 +26,8 @@ module.exports = {
     session: {
         cookieKey: 'thenetninjaisawesomeiguess'
     }
+<<<<<<< HEAD
 };
+=======
+};
+>>>>>>> 5a6025bb5736900c9ade03b0fabc67f4fd2b81bd
