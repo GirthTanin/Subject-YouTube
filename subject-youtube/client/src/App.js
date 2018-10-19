d39364660c2498f3c058feea676b881f7363f515
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-10-19 19:06:36 -0400

    I still need to get some css files connected

--- subject-youtube/client/src/App.js
+++ subject-youtube/client/src/App.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 
 import TestPage from "./components/pages/testPage";
 import Nav from './components/NavBar/Nav.js';
-import logo from './logo.svg';
+import Footer from "./components/footer/Footer";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      
       <div className="App">
+      <Nav />
         <header className="App-header">
+
           
           <TestPage />
+          
 
         </header>
+        <Footer />
       </div>
     );
   }
---
+++ subject-youtube/client/src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+    // this is where I need some basic html code for a footer
+    <footer class="container">
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+    );
+
+export default Footer;
