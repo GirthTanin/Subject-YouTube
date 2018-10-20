994a0aab75133a5b32819e2d266c561cec5165f3
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-10-19 20:54:12 -0400

    some styling to the navbar, it might be terrible...

--- subject-youtube/client/src/components/NavBar/Nav.js
+++ subject-youtube/client/src/components/NavBar/Nav.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+// for more tips on how we do more of this... https://github.com/linalockheart/nyt-react-search/blob/master/client/src/components/Nav/Nav.js
 
 const Nav = () => (
     // THIS WILL NEED TO BE TEASED OUT INTO THE CORRECT SET UP...
